4052543c417bda90ef6aa5c00ba0c6ee835154f2
nikakarelets <v.karelets@gmail.com>
2021-09-24 18:52:17 +0300

    added debounce

--- src/module-1/debounce/index.js
+++ src/module-1/debounce/index.js
@@ -1,5 +1,12 @@
 export const debounce = (fn, delay = 0) => {
+  let timeout;
 
-}
+  return function () {
+    const fnCall = () => {
+      console.log(fn.apply(this, arguments))
+    };
 
-
+    clearTimeout(timeout);
+    timeout = setTimeout(fnCall, delay)
+  }
+};
